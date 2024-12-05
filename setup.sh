47fea5ded7347818e8f4fb6b652220426c6f3091
Matthew Gibbons <mhgibbons@me.com>
2024-12-05 09:12:20 +0000

    Add digital audio transcription and analysis notebook; update setup script with new dependencies

--- setup.sh
+++ setup.sh
@@ -2,4 +2,7 @@
 pip install -q -r requirements.txt
 pip install -q beautifulsoup4 pandas requests
 pip install -q scrapy
+pip install -q openai openai-whisper
+pip install -q python-dotenv
+pip install -q python-docx
 pip freeze > requirements.txt
